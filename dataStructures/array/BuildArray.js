980c610b9af92309139d9710a5e0be4e63b901f6
Samuel <samuel_singer@apple.com>
2019-08-09 17:52:35 -0700

    pop, shift, unshift tested and complete

--- dataStructures/array/BuildArray.js
+++ dataStructures/array/BuildArray.js
@@ -1,5 +1,3 @@
-import { stat } from "fs";
-
 export default class BuildArray {
     constructor() {
         this.state = ['first', 'second', 'third']
@@ -9,10 +7,8 @@ export default class BuildArray {
         this.state.forEach((item, index) => console.log(index, ':', item))
     }
 
-    push(value) {
-        const stateLength = this.state.length;
-        
-        this.state[stateLength] = value;
+    push(value) {        
+        this.state = [...this.state, value]
     }
 
     pop() {
@@ -22,13 +18,31 @@ export default class BuildArray {
             return this.state
         } else {
             const poppedValue = this.state[stateLength -  1];
-            const newState = this.state.slice(0, state.length - 1);
+            const newState = this.state.slice(0, this.state.length - 1);
 
             this.state = newState;
             
             return poppedValue;
         }
     }
+
+    shift(value) {
+        this.state = [value, ...this.state]
+    }
+
+    unShift() {
+        if (!this.state) {
+            return []
+        } else {
+            const unShiftedValue = this.state[0];
+            const newState = this.state.slice(1);
+
+            this.state = newState;
+
+            return unShiftedValue;
+        }
+    }
+
 }
 
 
